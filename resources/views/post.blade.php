04ea30fbfe743c66fae59cfeb2e43a92fcda328a
niel2512 <nielyusuf17@gmail.com>
2025-05-14 22:01:12 +0700

    Edit Foreach dari p ke post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,10 @@
   <x-layout :title="$title">
       <article class="py-9 max-w-screen-md">
-      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 ">{{ $p['title'] }}</h2>
+      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 ">{{ $post['title'] }}</h2>
       <div class="text-base text-gray-500">
-        <a href="#">{{ $p['author'] }}</a> | 14 May 2025
+        <a href="#">{{ $post['author'] }}</a> | 14 May 2025
       </div>
-      <p class="my-4 font-light">{{ $p['body']}}</p>
+      <p class="my-4 font-light">{{ $post['body']}}</p>
       <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to all posts.</a>
     </article>
   </x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,17 +2,17 @@
     {{-- dd untuk debugging array--}}
     {{-- @dd($posts)  --}}
     {{-- Disini bakalan mengisi $slot di layout --}}
-    @foreach ($posts as $p)
+    @foreach ($posts as $post)
     {{-- Looping untuk mengambil data array di route --}}
     <article class="py-9 max-w-screen-md border-b border-gray-300">
-      <a href="/posts/{{ $p['id'] }}">
-      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:underline">{{ $p['title'] }}</h2></a>
+      <a href="/posts/{{ $post['id'] }}">
+      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:underline">{{ $post['title'] }}</h2></a>
       <div class="text-base text-gray-500">
-        <a href="#">{{ $p['author'] }}</a> | 14 May 2025
+        <a href="#">{{ $post['author'] }}</a> | 14 May 2025
       </div>
       {{-- Menggunakan helper string limit 10 karakter--}}
-      <p class="my-4 font-light">{{ Str::limit($p['body']),10 }}</p>
-      <a href="/posts/{{ $p['id'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+      <p class="my-4 font-light">{{ Str::limit($post['body']),10 }}</p>
+      <a href="/posts/{{ $post['id'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
       </article>
       @endforeach
   </x-layout>
