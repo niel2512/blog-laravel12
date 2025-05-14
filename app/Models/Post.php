9e091b6b68919518778e326bc0568f5c31adbc71
niel2512 <nielyusuf17@gmail.com>
2025-05-14 23:15:05 +0700

    Membuat relasi antar table di model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 //menggunakan model binding 
 class Post extends Model 
 {
@@ -11,4 +13,9 @@ class Post extends Model
  // protected $table = 'Post'; //ini digunakan kalau table kita tidak menggunakan default table laravel 'Posts' ada s nya
  use HasFactory; //agar bisa memanggil faker di tinker
  protected $fillable = ['title','author','slug','body']; //ini untuk memperbolehkan table tersebut diisi 
+
+ public function author(): BelongsTo
+ {
+  return $this->belongsTo(User::class);
+ }
 }
