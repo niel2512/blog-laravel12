a8edeefcb31aaee750e2cfdfc4fedc841f18c7ca
niel2512 <nielyusuf17@gmail.com>
2025-05-15 00:14:10 +0700

    Add blade-icons

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -23,7 +23,8 @@
                 <button type="button" @click="isOpen = !isOpen" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span class="absolute -inset-1.5"></span>
                   <span class="sr-only">Open user menu</span>
-                  <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                  
+                  <div class="size-8 rounded-full text-white"><x-ei-user /></div>
                 </button>
               </div>
               <div x-show="isOpen" x-transition:enter="transition ease-out duration-100 transform"
