8eea942a7f4a9ee1b0660296d450e55d14983708
niel2512 <nielyusuf17@gmail.com>
2025-05-16 02:18:21 +0700

    Menghapus Route Category dan Author

--- routes/web.php
+++ routes/web.php
@@ -9,22 +9,13 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
 Route::get('/posts', function () {
-    $posts = Post::latest()->filter(request(['search','category']))->get();
+    $posts = Post::latest()->filter(request(['search','category', 'author']))->get();
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 // Route model binding {namamodel:slug} custom model binding untuk mencari berdasarkan slug
 Route::get('/posts/{post:slug}', function (Post $post){
     return view('post', ['title' => 'Blog Details', 'post' => $post]);
-});
-
-// Route model binding {namamodel:username} 
-Route::get('/authors/{user:username}', function (User $user){
-    return view('posts', ['title' => count($user->posts) . ' Article by '. $user->name, 'posts' => $user->posts]);
-});
-// Route model binding {namamodel:slug} 
-Route::get('/categories/{category:slug}', function (Category $category){
-    return view('posts', ['title' => 'Category: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/about', function () {
