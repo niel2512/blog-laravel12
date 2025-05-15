14a6bfb7bc119b8d1a60c79eb7ff6c34eb499d5b
niel2512 <nielyusuf17@gmail.com>
2025-05-15 21:52:28 +0700

    Create UI Blog

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,15 +15,18 @@ class CategorySeeder extends Seeder
     {
         Category::create([
             'name' => 'Web Design',
-            'slug' => 'web-design'
+            'slug' => 'web-design',
+            'color' => 'bg-red-100'
         ]);
         Category::create([
             'name' => 'Web Programming',
-            'slug' => 'web-programming'
+            'slug' => 'web-programming',
+            'color' => 'bg-green-100'
         ]);
         Category::create([
             'name' => 'Artificial Intelligence',
-            'slug' => 'ai'
+            'slug' => 'ai',
+            'color' => 'bg-blue-100'
         ]);
     }
 }
