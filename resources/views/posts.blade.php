969fe4a6a55a3ee1b55b10ee1062137053f4fa3e
niel2512 <nielyusuf17@gmail.com>
2025-05-16 11:39:47 +0700

    Mengubah foreach jadi forelse agar bisa menambah empty message

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -25,7 +25,7 @@
       </form>
       {{ $posts->links() }}
       <div class="mb-4 mt-4 grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-        @foreach ($posts as $post)
+        @forelse ($posts as $post)
         <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
           <div class="flex justify-between items-center mb-5 text-gray-500">
             <a href="/posts?category={{ $post->category->slug }}">
@@ -51,8 +51,16 @@
               <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
           </div>
-        </article> 
-          @endforeach               
+        </article>
+        {{-- menambah empty agar menampilkan isi jika tidak terdapat blog yang dicari --}}
+        @empty
+        <div class="flex justify-center items-center min-w-screen">
+          <div class="w-full max-w-xs bg-slate-300 rounded-lg p-6 text-center">
+            <p class="font-semibold text-xl my-4">Article Not Found!</p>
+            <a href="/posts" class="text-blue-500 hover:underline">&laquo; Back to all blog</a>
+          </div>
+        </div>
+        @endforelse             
         </div>  
         {{-- {{ $posts->links() }} --}}
   </div>
